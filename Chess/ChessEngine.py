f2c6d3a8560fe40b7f57f90181b358b962ab8a60
Rahul Mewada <rahulmewada95@gmail.com>
2021-01-21 15:06:03 -0500

    Added a move class

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -30,5 +30,31 @@ class GameState():
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
             ]) 
-        self.whiteToMove = True
-        self.moveLog = np.array([])
+        self.white_to_move = True
+        self.move_log = np.array([])
+
+    def make_move(self, move):
+        self.board[move.start_row][move.start_row] = ".."
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.move_log.append(move)
+        self.white_to_move = not white_to_move
+
+class Move():
+    rank_to_row = {"8": 0, "7": 1, "6": 2, "5": 3, "4": 4, "3": 5, "2": 6, "1": 7}
+    row_to_rank = {val: key for key, val in rank_to_row.items()}
+    file_to_col = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 8}
+    col_to_file = {val: key for key, val in file_to_col.items()}
+
+    def __init__(self, start_sq, end_sq, board):
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+    def get_simple_chess_notation(self):
+        return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
+
+    def get_rank_file(self, row, col):
+        return self.col_to_file[col] + self.row_to_rank[row]
