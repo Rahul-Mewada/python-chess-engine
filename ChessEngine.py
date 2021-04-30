e3e0766871dbd59312a5f020cb5fb40634b8f0ea
Rahul Mewada <rahulmewada95@gmail.com>
2021-04-30 14:34:51 -0400

    Added unit tests

--- ChessEngine.py
+++ ChessEngine.py
@@ -10,23 +10,25 @@ class GameState():
     def fen_to_board(self, fen_string):
         """
         Takes a Forsyth-Edwards Notation string and returns the
-        corresponding board representation
+        corresponding board representation, and other game state
+        information
         """
         board = [[Pieces.EmptyPiece(row, col) for col in range(8)]
                  for row in range(8)]
         row, col = 0, 0
         for char in fen_string:
             if char.isnumeric():
-                for i in range(int(char)):
-                    row = i
-                    board[row][col] = Pieces.EmptyPiece(row, col)
+                row += int(char)
+                if row >= 8:
+                    row = 0
             elif char == '/':
                 col += 1
                 row = 0
             else:
-                color = 'w' if char.isupper() else 'b'
+                color = 'white' if char.isupper() else 'black'
                 piece = self.args_to_piece(char.lower(), row, col, color)
                 board[row][col] = piece
+                row += 1
         return board
 
     def args_to_piece(self, key, row, col, color):
--- test_ChessEngine.py
+++ test_ChessEngine.py
@@ -1,6 +1,66 @@
+import ChessEngine
+import Pieces
 
 
 class TestGameState():
 
     def test_fen_to_board():
-        pass
+        target_boards = [
+            [
+                [('black', 'r'), '.', '.', '.', ('black', 'r'), '.', ('black', 'k'), '.'],
+                [('black', 'p'), ('black', 'p'), '.', '.', '.', ('black', 'n'), ('white', 'p'), ('black', 'p')],
+                ['.', ('black', 'b'), '.', ('black', 'p'), '.', ('white', 'b'), '.', '.'],
+                ['.', ('black', 'q'), '.', ('white', 'p'), '.', ('white', 'n'), '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.'],
+                [('white', 'p'), '.', '.', '.', '.', ('black', 'q'), '.', '.'],
+                ['.', ('white', 'p'), '.', '.', '.', ('white', 'p'), ('white', 'q'), '.'],
+                [('white', 'r'), '.', '.', '.', '.', '.', '.', ('white', 'r')]
+            ],
+
+            [
+                [('black', 'r'), ('black', 'n'), ('black', 'b'), ('black', 'q'), ('black', 'k'),
+                    ('black', 'b'), ('black', 'n'), ('black', 'r')],
+                [('black' 'p'), ('black', 'p'), ('black', 'p'), ('black', 'p'), ('black', 'p'),
+                    ('black', 'p'), ('black', 'p'), ('black', 'p')],
+                ['.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.'],
+                ['.', '.', '.', '.', '.', '.', '.', '.'],
+                [('white', 'p'), ('white', 'p'), ('white', 'p'), ('white', 'p'), ('white', 'p'),
+                    ('white', 'p'), ('white', 'p'), ('white', 'p')],
+                [('white', 'r'), ('white', 'n'), ('white', 'b'), ('white', 'q'), ('white', 'k'),
+                    ('white', 'b'), ('white', 'n'), ('white', 'r')]
+            ]
+        ]
+        letter_to_piece = {
+            'p': Pieces.Pawn,
+            'r': Pieces.Rook,
+            'n': Pieces.Knight,
+            'b': Pieces.Bishop,
+            'q': Pieces.Queen,
+            'k': Pieces.King
+        }
+        fen_strings = ['r3r1k1/pp3nPp/1b1p1B2/1q1P1N2/8/P4Q2/1P3PK1/R6R',
+                       'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR']
+
+        for index, target_board in target_boards:
+            target_fen_string = fen_strings[index]
+            result_board = ChessEngine.fen_to_board(target_fen_string)
+            for row in range(8):
+                for col in range(8):
+                    result_piece = result_board[row][col]
+                    target_piece = target_board[row][col]
+                    if target_piece == '.':
+                        if result_piece.type() is not Pieces.EmptyPiece:
+                            return False
+                    else:
+                        target_piece_color, target_piece_type = target_piece
+                        if result_piece.color != target_piece_color:
+                            return False
+                        if result_piece.type() is not \
+                                letter_to_piece[target_piece_type]:
+                            return False
+                    if result_piece.row != row or result_piece.col != col:
+                        return False
+
+        return True
