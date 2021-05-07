7425e5a3c4d1aaffd033f43b739d99b01473b05d
Rahul Mewada <rahulmewada95@gmail.com>
2021-05-07 13:09:48 -0400

    Added validate board func

--- utils.py
+++ utils.py
@@ -8,3 +8,15 @@ def in_bounds(square):
     if 0 <= row < 8 and 0 <= col < 8:
         return True
     return False
+
+
+def valid_board(board):
+    """
+    Returns true if the pieces match their position on the board
+    """
+    for row in range(8):
+        for col in range(8):
+            piece = board[row][col]
+            if piece.row != row or piece.col != col:
+                return False
+    return True
