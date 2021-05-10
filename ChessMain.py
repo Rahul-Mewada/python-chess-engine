96c0c9f99f5c7db0b54af4cda34e942c15ef41c0
Rahul Mewada <rahulmewada95@gmail.com>
2021-05-10 13:43:43 -0400

    Added move animations

--- ChessMain.py
+++ ChessMain.py
@@ -49,6 +49,7 @@ def main():
     state = ChessEngine.GameState()
     valid_moves = state.get_valid_moves()
     move_made = False
+    animate = False
     load_images()
     # keeps track of the last click of the user
     sq_selected = ()
@@ -76,6 +77,7 @@ def main():
                     for m in valid_moves:
                         if m.id == move.id:
                             move_made = True
+                            animate = True
                             state.make_move(move, True)
                     sq_selected = ()
                     player_clicks = []
@@ -83,9 +85,13 @@ def main():
                 if event.key == pygame.K_z:
                     state.undo_move()
                     move_made = True
+                    animate = False
         if move_made:
+            if animate:
+                animate_move(state.move_log[-1], screen, state.board, clock)
             valid_moves = state.get_valid_moves()
             move_made = False
+            animate = False
         draw_gamestate(screen, state, valid_moves, sq_selected)
         clock.tick(MAX_FPS)
         pygame.display.flip()
@@ -112,6 +118,34 @@ def highlight_squares(screen, state, valid_moves, sq_selected):
                 if start_row == r and start_col == c:
                     screen.blit(s, (SQ_SIZE * end_col,
                                     SQ_SIZE * end_row))
+
+
+def animate_move(move, screen, board, clock):
+    """ Animates the piece moves """
+    colors = [pygame.Color("white"), pygame.Color("gray")]
+    start_row, start_col = move.start_sq
+    end_row, end_col = move.end_sq
+    d_row, d_col = end_row - start_row, end_col - start_col
+    frames_per_sq = 10
+    frame_count = (abs(d_row) + abs(d_col)) * frames_per_sq
+    for frame in range(frame_count + 1):
+        row, col = (start_row + (d_row * frame/frame_count),
+                    start_col + (d_col * frame/frame_count))
+        draw_board_squares(screen)
+        draw_pieces(screen, board)
+        color = colors[(end_row + end_col) % 2]
+        end_square = pygame.Rect(end_col * SQ_SIZE, end_row * SQ_SIZE,
+                                 SQ_SIZE, SQ_SIZE)
+        pygame.draw.rect(screen, color, end_square)
+        if not move.piece_removed.is_empty:
+            piece_removed_image = piece_to_image(move.piece_removed)
+            screen.blit(piece_removed_image, end_square)
+        piece_moved_image = piece_to_image(move.piece_to_move)
+        screen.blit(piece_moved_image,
+                    pygame.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+                    )
+        pygame.display.flip()
+        clock.tick(60)
 
 
 def draw_gamestate(screen, state, valid_moves, sq_selected):
